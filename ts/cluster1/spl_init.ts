cf545cf080b8581e3af4d725b9c8ba5e87dd7570
HarshS1611 <harshsingh16d@gmail.com>
2026-01-28 20:19:59 +0530

    feat: minted tokens on devnet

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import wallet from "/Users/harshsingh/.config/solana/id.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -11,8 +11,17 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
     try {
-        // Start here
-        // const mint = ???
+        // Mint tokens
+        const mint = await createMint(
+            connection,
+            keypair,
+            keypair.publicKey,
+            null,
+            6
+        )
+
+        console.log("Minted token address: ", mint.toBase58())
+
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
